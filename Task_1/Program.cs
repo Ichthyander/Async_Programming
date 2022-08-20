ea4857ff46527e040ad49366c6b98a215d2a273c
Ichtyander <di_ignatov@mail.ru>
2022-08-20 23:59:52 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1.    Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.*/
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для вычисления факториала");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(Factorial(n));
+
+            FactorialAsync(n);
+            
+            //Проверка асинхронности
+            Console.WriteLine("Метод Main завершает работу");
+            Console.ReadKey();
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        static async Task<int> FactorialAsync(int n)
+        {
+            int factorial = await Task.Run(()=>Factorial(n));
+
+            //Вывод организован в теле метода, чтобы в методе Main к FactorialAsync(n) не ставить блокирующий параметр Result
+            Console.WriteLine("Факториал числа {0} равен - {1}",n, factorial);
+            return factorial;
+        }
+    }
+}
